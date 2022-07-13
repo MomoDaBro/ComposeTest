a92e5e1d8ebfd2fc15f496e5f9e244c0320b6718
MomoDaBro <Momofqais@gmail.com>
2022-07-12 22:11:20 -0500

    Added auto resizing and centering for the rectangle

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -5,31 +5,41 @@ import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.key.*
 
+
+@OptIn(ExperimentalComposeUiApi::class)
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Compose for Desktop",
-        state = rememberWindowState(width = 300.dp, height = 300.dp)
+        state = rememberWindowState(width = 500.dp, height = 500.dp)
     ) {
         Canvas(modifier = Modifier.fillMaxSize()) {
             val canvasWidth = size.width
             val canvasHeight = size.height
 
-//            for (delta in 0..10) {
-//                drawLine(
-//                    start = Offset(x = 0f, y = 0f),
-//                    end = Offset(x = 0f, y = canvasHeight),
-//                    color = Color.Blue
-//                )
+//                .onKeyEvent(onKeyEvent = ::foo)
 
             drawRect(
-                color = Color.Red
+                color = Color.Gray,
+                topLeft = Offset(canvasWidth / 4, canvasHeight / 4),
+                size = Size(canvasWidth / 2, canvasHeight / 2)
             )
         }
 
     }
 }
+
+fun foo(keyEvent: KeyEvent): Boolean{
+    println(keyEvent.key)
+    return true
+}
+
+
+
