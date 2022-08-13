63c22a46658b3f65fbaaff108f53c81e898e70c6
MomoDaBro <Momofqais@gmail.com>
2022-08-13 01:30:25 -0500

    Created the drawCell function and used it to reconstruct the addYellowSquare function, Also used translate for the gridstarts and used DrawScopes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -23,6 +23,10 @@ const val CELL_SIZE = 40f
 const val NUM_COL = 10
 const val NUM_ROW = 20
 
+// UI consts
+const val CANVAS_HEIGHT = 1000
+const val CANVAS_WIDTH = 1000
+
 // Other
 const val CELL_BORDER_SIZE = 8
 val Orange get() = Color(red = 255, green = 165, blue = 0)
@@ -30,6 +34,7 @@ fun main() = app()
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun app() = application {
+    val shiftDownTimer = remember { mutableStateOf(0) }
     val offsetHorizontal = remember { mutableStateOf(0) }
 
     LaunchedEffect(Unit) {
@@ -42,7 +47,7 @@ fun app() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "Compose for Desktop",
-        state = rememberWindowState(width = canvasWidth.dp, height = canvasHeight.dp),
+        state = rememberWindowState(width = CANVAS_WIDTH.dp, height = CANVAS_HEIGHT.dp),
         onKeyEvent = {
 
             if (it.key == Key.DirectionRight) {
@@ -51,12 +56,6 @@ fun app() = application {
 
             if (it.key == Key.DirectionLeft) {
                 offsetHorizontal.value -= 40
-            }
-            if (it.key == Key.DirectionDown) {
-                offsetVertical.value += 40
-            }
-            if (it.key == Key.DirectionUp) {
-                offsetVertical.value -= 40
             }
             true
         }
